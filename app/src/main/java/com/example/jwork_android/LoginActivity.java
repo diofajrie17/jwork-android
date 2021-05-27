1f6e773d79c34421ccecef135d040c3c318684fb
Dio Fajrie <diofajriefadlullah@gmail.com>
2021-05-27 17:57:25 +0700

    Modul9 - CS

--- app/src/main/java/com/example/jwork_android/LoginActivity.java
+++ app/src/main/java/com/example/jwork_android/LoginActivity.java
@@ -1,11 +1,21 @@
 package com.example.jwork_android;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,24 +33,33 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
-                if(email.equals("test@test.com") &&
-                        password.equals("test")){
-                    Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(LoginActivity.this,"Login Failed",
-                            Toast.LENGTH_SHORT).show();
-                }
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject != null) {
+                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                            }
+                        }catch (JSONException e) {
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
 
-        tvRegister.setOnClickListener(new View.OnClickListener(){
+        tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
+    }
 
 }
--- app/src/main/java/com/example/jwork_android/RegisterActivity.java
+++ app/src/main/java/com/example/jwork_android/RegisterActivity.java
@@ -1,33 +1,127 @@
 package com.example.jwork_android;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText etRegisterName, etRegisterEmail, etRegisterPassword;
+    private Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTransparentStatusBarOnly(RegisterActivity.this);
+        try
+        {
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        }
+        catch (NullPointerException ignored){};
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final EditText etRegisterName = findViewById(R.id.etRegisterName);
-        final EditText etRegisterEmail = findViewById(R.id.etRegisterEmail);
-        final EditText etRegisterPassword = findViewById(R.id.etRegisterPassword);
-        Button btnRegister = findViewById(R.id.btnRegister);
+        etRegisterName  = findViewById(R.id.etRegisterName);
+        etRegisterEmail = findViewById(R.id.etRegisterEmail);
+        etRegisterPassword = findViewById(R.id.etRegisterPassword);
+        btnRegister = findViewById(R.id.btnRegister);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String regexEmail = "^([\\w\\&\\_~]+\\.{0,1})+@[\\w][\\w\\-](\\.[\\w\\-]+)+$";
+                String regexPassword = "^(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{6,}$";
+
                 String name = etRegisterName.getText().toString();
                 String email = etRegisterEmail.getText().toString();
                 String password = etRegisterPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    etRegisterEmail.setError("Email is Required.");
+                    btnRegister.setEnabled(true);
+                    return;
+                }
+
+                if(TextUtils.isEmpty(name)){
+                    etRegisterName.setError("Name is Required.");
+                    btnRegister.setEnabled(true);
+                    return;
+                }
+
+                if(!Pattern.matches(regexEmail, email)){
+                    etRegisterPassword.setError("Email not valid.");
+                    btnRegister.setEnabled(true);
+                    return;
+                }
+
+                if(!Pattern.matches(regexPassword, password)){
+                    etRegisterPassword.setError("Password not valid.");
+                    btnRegister.setEnabled(true);
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    etRegisterPassword.setError("Password is Required.");
+                    btnRegister.setEnabled(true);
+                    return;
+                }
+
+                if(password.length() < 6){
+                    etRegisterPassword.setError("Password must be > 6 characters");
+                    btnRegister.setEnabled(true);
+                    return;
+                }
+
+                else{
+                    btnRegister.setEnabled(false);
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject != null){
+                                Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(name, email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
             }
         });
+    }
 
+    public void setTransparentStatusBarOnly(Activity activity) {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        // this lines ensure only the status-bar to become transparent without affecting the nav-bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
